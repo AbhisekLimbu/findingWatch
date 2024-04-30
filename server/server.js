b4f2e180f1159a78e5e20c347e6df3430af1b2b8
Abhisek Phago limbu <112129568+AbhisekLimbu@users.noreply.github.com>
2024-04-30 00:08:48 -0500

    checking server

--- server/server.js
+++ server/server.js
@@ -1,30 +1,25 @@
 const express = require('express');
-const { OpenAI } = require("openai"); // Import OpenAI from "openai"
+const axios = require('axios');
+const { OpenAI } = require("openai");
 const port = process.env.PORT || 3001;
 
-// Load environment variables from .env file
 require("dotenv").config();
 
-// Create Express app
 const app = express();
-
 app.use(express.json());
 
-// Initialize OpenAI with API key
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-// Define route for handling POST requests to /find-complexity
 app.get('/find-complexity', async (req, res) => {
   try {
-    // Handling for the "find-complexity" endpoint goes here
     const response = await openai.chat.completions.create({
       model: "gpt-3.5-turbo",
       messages: [
         {
           "role": "user",
-          "content": "fruits name" // Ensure that userInput is not null
+          "content": "1 which published in year 2011, korean Drama Name"
         }
       ],
       temperature: 1,
@@ -34,23 +29,38 @@ app.get('/find-complexity', async (req, res) => {
       presence_penalty: 0,
     });
 
-    // Send the response from OpenAI chat completions back to the client
-    return res.json({ response: response.choices[0].message.content });
+    const movieName = response.choices[0].message.content;
+    console.log("Movie Name:", movieName);
+
+    // Fetch movie details from TMDB based on the movie name
+    const apiKey = process.env.TMDB_API_KEY;
+    const tmdbUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${encodeURIComponent(movieName)}`;
+
+    const tmdbResponse = await axios.get(tmdbUrl);
+    const movieData = tmdbResponse.data.results[0]; // Assuming the first result is the desired movie
+    
+    if (movieData) {
+      const posterPath = movieData.poster_path;
+      if (posterPath) {
+        const posterUrl = `https://image.tmdb.org/t/p/original/${posterPath}`;
+        return res.json({ posterUrl });
+      } else {
+        return res.status(404).json({ error: 'Poster not found' });
+      }
+    } else {
+      return res.status(404).json({ error: 'Movie not found' });
+    }
   } catch (error) {
-    console.error(error); // Log the error for debugging
-    return res.status(500).json({
-      error: "Something went wrong",
-    });
+    console.error('Error fetching movie details:', error);
+    return res.status(500).json({ error: 'Internal server error' });
   }
 });
 
-// Define a route for handling errors
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something went wrong!');
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
